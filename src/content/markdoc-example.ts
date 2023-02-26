3977ab9687fb2a55120b492ef9cb59cb811854b8
Oskar Hulter <stresslessness91@gmail.com>
2023-02-27 00:03:56 +0100

    - fix index content

---
+++ src/content/markdoc-example.ts
@@ -0,0 +1,8 @@
+import Markdoc from '@markdoc/markdoc'
+
+const source = '# Markdoc'
+
+const ast = Markdoc.parse(source)
+const content = Markdoc.transform(ast, /* config */)
+
+const html = Markdoc.renderers.html(content)
